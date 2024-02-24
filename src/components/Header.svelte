77f2ef43a6e8b4b5e41d6e0d16f37b368f20869a
Ayushman Mishra <132093864+Ayushman88@users.noreply.github.com>
2024-02-24 15:04:35 +0530

    landing page requirements met

---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { openModal } from "../store";
+</script>
+
+<header class="flex flex-col relative z-20">
+  <div
+    class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6"
+  >
+    <a
+      class="duration-200 text-xl hover:text-indigo-400 curson-pointer"
+      href="/"
+    >
+      <h1 class="font-semibold">
+        Gym <span class="text-indigo-400">Bricks</span>
+      </h1>
+    </a>
+    <button
+      on:click={() => ($openModal = true)}
+      class="md:hidden grid place-items-center"
+    >
+      <i class="fa-solid fa-bars"></i>
+      <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+        <a
+          class="duration-200 hover:text-indigo-400 curson-pointer"
+          href="#product">Product</a
+        >
+        <a
+          class="duration-200 hover:text-indigo-400 curson-pointer"
+          href="#reviews">Reviews</a
+        >
+        <a class="duration-200 hover:text-indigo-400 curson-pointer" href="#faq"
+          >FAQ'S
+        </a>
+        <button class="specialBtn">Start Free Today</button>
+      </nav>
+    </button>
+  </div>
+</header>
